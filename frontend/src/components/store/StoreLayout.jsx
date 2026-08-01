b739d2c9eec1c22791ce3fbffaac6deea21bcdde
prathamradadiya <pratham.webappdev@gmail.com>
2026-08-01 22:28:23 +0530

    feat: add initial loader and fade in animation

--- frontend/src/components/store/StoreLayout.jsx
+++ frontend/src/components/store/StoreLayout.jsx
@@ -5,7 +5,7 @@ import Footer from '../common/Footer';
 
 const StoreLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen animate-fade-in">
       <Navbar />
       <main className="flex-grow">
         <Outlet />
